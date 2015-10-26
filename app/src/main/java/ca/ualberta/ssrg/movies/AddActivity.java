a6f35148435d776ed9c2dcfd771849c111787b10
joshua2ua <joshua2@ualberta.ca>
2015-10-26 17:06:57 -0600

    Port to Andoid Studio

---
+++ app/src/main/java/ca/ualberta/ssrg/movies/AddActivity.java
@@ -0,0 +1,83 @@
+package ca.ualberta.ssrg.movies;
+
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import ca.ualberta.ssrg.androidelasticsearch.R;
+import ca.ualberta.ssrg.movies.es.Movie;
+import ca.ualberta.ssrg.movies.es.Movies;
+import ca.ualberta.ssrg.movies.es.MoviesController;
+
+public class AddActivity extends Activity {
+
+
+	// Thread that close the activity after finishing add
+	private Runnable doFinishAdd = new Runnable() {
+		public void run() {
+			finish();
+		}
+	};
+
+	private MoviesController moviesController;
+	private Movies movies;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add);
+		movies = new Movies();
+		moviesController = new MoviesController(movies);
+	}
+
+	public void save(View view) {
+		TextView id = (TextView) findViewById(R.id.detailsIdText);
+		TextView title = (TextView) findViewById(R.id.detailsTitleText);
+		TextView director = (TextView) findViewById(R.id.detailsDirectorText);
+		TextView year = (TextView) findViewById(R.id.detailsYearText);
+		TextView genre = (TextView) findViewById(R.id.detailsGenreText);
+		
+		// Create movie object
+		Movie newMovie = new Movie();
+		newMovie.setId(Integer.parseInt(id.getText().toString()));
+		newMovie.setTitle(title.getText().toString());
+		newMovie.setDirector(director.getText().toString());
+		newMovie.setYear(Integer.parseInt(year.getText().toString()));
+		
+		String genresString = genre.getText().toString();
+		String[] genresArray = genresString.split(",");
+		List<String> genres = Arrays.asList(genresArray);
+		newMovie.setGenres(genres);
+		
+		// Execute the thread
+		Thread thread = new AddThread(newMovie);
+		thread.start();
+	}
+	
+	class AddThread extends Thread {
+		private Movie movie;
+
+		public AddThread(Movie movie) {
+			this.movie = movie;
+		}
+
+		@Override
+		public void run() {
+			moviesController.addMovie(movie);
+			
+			// Give some time to get updated info
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			runOnUiThread(doFinishAdd);
+		}
+	}
+}
+
+
---
+++ app/src/main/java/ca/ualberta/ssrg/movies/MainActivity.java
@@ -0,0 +1,161 @@
+package ca.ualberta.ssrg.movies;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import ca.ualberta.ssrg.androidelasticsearch.R;
+import ca.ualberta.ssrg.movies.es.ESMovieManager;
+import ca.ualberta.ssrg.movies.es.Movie;
+import ca.ualberta.ssrg.movies.es.Movies;
+import ca.ualberta.ssrg.movies.es.MoviesController;
+
+public class MainActivity extends Activity {
+
+	private ListView movieList;
+	private Movies movies;
+	private ArrayAdapter<Movie> moviesViewAdapter;
+	private ESMovieManager movieManager;
+	private MoviesController moviesController;
+
+	private Context mContext = this;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		movieList = (ListView) findViewById(R.id.movieList);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+
+		movies = new Movies();
+		moviesViewAdapter = new ArrayAdapter<Movie>(this, R.layout.list_item,movies);
+		movieList.setAdapter(moviesViewAdapter);
+		movieManager = new ESMovieManager("");
+
+		// Show details when click on a movie
+		movieList.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int pos,	long id) {
+				int movieId = movies.get(pos).getId();
+				startDetailsActivity(movieId);
+			}
+
+		});
+
+		// Delete movie on long click
+		movieList.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+				Movie movie = movies.get(position);
+				Toast.makeText(mContext, "Deleting " + movie.getTitle(), Toast.LENGTH_LONG).show();
+
+				Thread thread = new DeleteThread(movie.getId());
+				thread.start();
+
+				return true;
+			}
+		});
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		
+
+		// Refresh the list when visible
+		// TODO: Search all
+		
+	}
+	
+	/** 
+	 * Called when the model changes
+	 */
+	public void notifyUpdated() {
+		// Thread to update adapter after an operation
+		Runnable doUpdateGUIList = new Runnable() {
+			public void run() {
+				moviesViewAdapter.notifyDataSetChanged();
+			}
+		};
+		
+		runOnUiThread(doUpdateGUIList);
+	}
+
+	/** 
+	 * Search for movies with a given word(s) in the text view
+	 * @param view
+	 */
+	public void search(View view) {
+		movies.clear();
+
+		// TODO: Extract search query from text view
+		
+		// TODO: Run the search thread
+		
+	}
+	
+	/**
+	 * Starts activity with details for a movie
+	 * @param movieId Movie id
+	 */
+	public void startDetailsActivity(int movieId) {
+		Intent intent = new Intent(mContext, DetailsActivity.class);
+		intent.putExtra(DetailsActivity.MOVIE_ID, movieId);
+
+		startActivity(intent);
+	}
+	
+	/**
+	 * Starts activity to add a new movie
+	 * @param view
+	 */
+	public void add(View view) {
+		Intent intent = new Intent(mContext, AddActivity.class);
+		startActivity(intent);
+	}
+
+
+	class SearchThread extends Thread {
+		// TODO: Implement search thread
+		
+	}
+
+	
+	class DeleteThread extends Thread {
+		private int movieId;
+
+		public DeleteThread(int movieId) {
+			this.movieId = movieId;
+		}
+
+		@Override
+		public void run() {
+			moviesController.deleteMovie(movieId);
+
+			// Remove movie from local list
+			for (int i = 0; i < movies.size(); i++) {
+				Movie m = movies.get(i);
+
+				if (m.getId() == movieId) {
+					movies.remove(m);
+					break;
+				}
+			}
+		}
+	}
+}
